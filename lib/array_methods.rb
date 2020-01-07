c7545ba92d91cd2cfb77a2459f5a4a2d6832a124
David Ricketts <david.ricketts119@gmail.com>
2020-01-07 16:06:44 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,28 @@
 def using_include(array, element)
-
+  array.include?(element)
 end
 
 def using_sort(array)
-
+  sorted_array = array.sort
+  sorted_array
 end
 
 def using_reverse(array)
-
+  reversed_array = array.reverse 
+  return reversed_array
 end
 
 def using_first(array)
-
+  first_element = array.first 
+  return first_element
 end
 
 def using_last(array)
-
+  last_element = array.last 
+  return last_element
 end
 
 def using_size(array)
-
+  size = array.size
+  return size
 end
